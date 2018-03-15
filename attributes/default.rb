233576cc5fd2127ef3224119704745073d6c2556
morblom <morblom@atex.com>
2018-03-15 08:22:47 +0100

    Added default attribute for solr5 version

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,4 @@ default['solr5']['port'] = 8983
 default['solr5']['home'] = default['solr5']['install_path'] + '/solr_home'
 default['solr5']['is_slave'] = false
 default['solr5']['is_master'] = false
+default['solr5']['version'] = '5.3.0'
