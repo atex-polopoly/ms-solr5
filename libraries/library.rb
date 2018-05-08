cd1633ac0c7c84f994b9651111ed02f2cfa76f7c
morblom <morblom@atex.com>
2018-05-08 13:07:05 +0200

    Changed is_slave to true as default. Modified parameter generation to only add master configuration if master and only add slave configuration if slave and not master.

--- libraries/library.rb
+++ libraries/library.rb
@@ -1,8 +1,10 @@
 def generate_parameters
+  is_master = node['solr5']['is_master']
+  is_slave = node['solr5']['is_slave'] && !node['solr5']['is_master']
   params = ''
-  params << " -Dsolr5.master.enabled=true" if node['solr5']['is_master']
-  params << " -Dsolr5.slave.enabled=true" if node['solr5']['is_slave']
-  params << " -Dsolr5.master.host=#{node['solr5']['master']['host']}" if node['solr5']['is_slave']
-  params << " -Dsolr5.master.port=#{node['solr5']['master']['port']}" if node['solr5']['is_slave']
+  params << " -Dsolr5.master.enabled=true" if is_master
+  params << " -Dsolr5.slave.enabled=true" if is_slave
+  params << " -Dsolr5.master.host=#{node['solr5']['master']['host']}" if is_slave
+  params << " -Dsolr5.master.port=#{node['solr5']['master']['port']}" if is_slave
   params
 end
